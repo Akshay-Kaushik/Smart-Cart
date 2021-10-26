edf46264347ddea0bc575488833a555bd3cd2509
Akshay <agkblr2001@gmail.com>
2021-10-26 22:58:53 +0530

    login/signup fix

--- app/src/main/java/com/example/smartcart/SignupTabFragment.java
+++ app/src/main/java/com/example/smartcart/SignupTabFragment.java
@@ -1,22 +1,44 @@
 package com.example.smartcart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupTabFragment extends Fragment {
     EditText firstname,lastname,email,phone,pass,confirm;
     Button signup;
     float v=0;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    String full_name,password,phone_number,mail;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    String MobilePattern = "[0-9]{10}";
     @Override
     public View onCreateView(@org.jetbrains.annotations.NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup root =(ViewGroup) inflater.inflate(R.layout.signup_tab_fragment,container, false);
-
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         firstname=root.findViewById(R.id.firstname);
         lastname=root.findViewById(R.id.lastname);
         phone=root.findViewById(R.id.phone);
@@ -24,14 +46,14 @@ public class SignupTabFragment extends Fragment {
         pass=root.findViewById(R.id.pass);
         confirm=root.findViewById(R.id.confirm);
         signup=root.findViewById(R.id.signup);
-
-//        firstname.setTranslationX(800);
-//        lastname.setTranslationX(800);
-//        phone.setTranslationX(800);
-//        email.setTranslationX(800);
-//        pass.setTranslationX(800);
-//        confirm.setTranslationX(800);
-//        signup.setTranslationX(800);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validate()){
+                    firebase_register();
+                }
+            }
+        });
 
         firstname.setAlpha(v);
         lastname.setAlpha(v);
@@ -50,5 +72,86 @@ public class SignupTabFragment extends Fragment {
         signup.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(700).start();
 
         return root;
+    }
+    public boolean validate() {
+        full_name = firstname.getText().toString() + " " + lastname.getText().toString();
+        phone_number = phone.getText().toString();
+        mail = email.getText().toString();
+        password = pass.getText().toString();
+        Log.d("a", full_name);
+
+        if (full_name.length() == 1) {
+            firstname.setError("Name cannot be empty");
+            lastname.setError("Name cannot be empty");
+            return false;
+        } else if (!phone_number.matches(MobilePattern)) {
+            phone.setError("Enter Correct Mobile Number");
+            return false;
+        } else if (!mail.trim().matches(emailPattern)) {
+            email.setError("Enter Correct Email");
+            return false;
+        } else if (password.isEmpty()) {
+            pass.setError("Password cannot be empty");
+            return false;
+        } else if (password.length() < 6) {
+            pass.setError("Password should contain atleast 6characters");
+        } else if (confirm.getText().toString().isEmpty()) {
+            confirm.setError("Confirm your password");
+            return false;
+        } else if (!password.equals(confirm.getText().toString())) {
+            confirm.setError("Make sure you confirm your password correctly!");
+            return false;
+        } else {
+            return true;
+        }
+
+        return false;
+    }
+    public void firebase_register()
+    {
+        mAuth.createUserWithEmailAndPassword(mail, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.d("abc",mAuth.toString());
+                            Toast.makeText(getContext(), "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                            store_info();
+                            user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(getContext(), "Verification Email Sent !", Toast.LENGTH_LONG).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            Intent intent = new Intent(getContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getContext(),"Email already registered or some error occurred!",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    public void store_info() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("FName", full_name);
+        user.put("Email", email);
+        user.put("Phone", phone_number);
+// Add a new document with a generated ID
+        db.collection("users")
+                .document(mAuth.getUid())
+                .set(user);
     }
 }
--- app/src/main/java/com/example/smartcart/loginActivity.java
+++ app/src/main/java/com/example/smartcart/loginActivity.java
@@ -19,7 +19,6 @@ public class loginActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
-    FloatingActionButton fb,google,twitter;
     LoginAdapter adapter;
     float v=0;
 
@@ -31,9 +30,7 @@ public class loginActivity extends AppCompatActivity {
 
         tabLayout=findViewById(R.id.tab_layout);
         viewPager=findViewById(R.id.view_pager);
-//        fb=findViewById(R.id.fab_fb);
-//        google=findViewById(R.id.fab_google);
-//        twitter=findViewById(R.id.fab_twitter);
+
 
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("Signup"));
@@ -44,20 +41,11 @@ public class loginActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-//        fb.setTranslationY(300);
-//        google.setTranslationY(300);
-//        twitter.setTranslationY(300);
+
         tabLayout.setTranslationY(300);
 
-
-//        fb.setAlpha(v);
-//        google.setAlpha(v);
-//        twitter.setAlpha(v);
         tabLayout.setAlpha(v);
 
-//        fb.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-//        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
-//        twitter.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
         tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
 
     }
